5eefec674eff5ca217286bbe5bbaaf9ba954f01b
wwampirs <dima.tchernenko@gmail.com>
2018-02-12 17:46:36 +0200

    Add Controller Navigation

--- STO.WebUI/Controllers/NavigationController.cs
+++ STO.WebUI/Controllers/NavigationController.cs
@@ -23,14 +23,19 @@ namespace STO.WebUI.Controllers
 
         public ActionResult GetTotalPrices()
         {
-            List<TotalPrice> totalPrices = db.TotalPrices.Include(c => c.Car).Take(5).ToList();
+            List<TotalPrice> totalPrices = db.TotalPrices
+                .Include(c => c.Car).Take(5).ToList();
             return PartialView(totalPrices);
         }
 
         public ActionResult Details(int id)
         {
 
-            TotalPrice totalPrice = db.TotalPrices.Include(c => c.Car).Include(l => l.CalculateCost).Where(p => p.Id == id).FirstOrDefault();
+            TotalPrice totalPrice = db.TotalPrices
+                .Include(c => c.Car)
+                .Include(l => l.CalculateCost)
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
             return View(totalPrice);
         }
     }

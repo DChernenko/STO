dd5a3369513f283cda950a2836f5d7cab81145c9
Alex Truba <straber@ukr.net>
2018-07-07 13:28:14 +0300

    generate forms view and viewmodel class

---
+++ STO.WebUI/Models/BaseCarViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace STO.WebUI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class BaseCarViewModel
+    {
+        [Required(ErrorMessage = "Поле обов'язково для заповнення")]
+        [Range(0, 100, ErrorMessage = "Значення повино бути в межах від 0 до 100")]
+        public int CarCase { get; set; }
+
+        [Required(ErrorMessage = "Поле обов'язково для заповнення")]
+        [Range(0, 100, ErrorMessage = "Значення повино бути в межах від 0 до 100")]
+        public int Wheel { get; set; }
+
+        [Required(ErrorMessage = "Поле обов'язково для заповнення")]
+        [Range(0, 100, ErrorMessage = "Значення повино бути в межах від 0 до 100")]
+        public int Engine { get; set; }
+
+        [Required(ErrorMessage = "Поле обов'язково для заповнення")]
+        [Range(0, 100, ErrorMessage = "Значення повино бути в межах від 0 до 100")]
+        public int Brake { get; set; }
+
+        [Required(ErrorMessage = "Поле обов'язково для заповнення")]
+        [Range(0, 100, ErrorMessage = "Значення повино бути в межах від 0 до 100")]
+        public int Undercarriage { get; set; }
+    }
+}
---
+++ STO.WebUI/Models/BusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STO.WebUI.Models
+{
+    public class BusViewModel : BaseCarViewModel
+    {
+        public int Handrail { get; set; }
+        public int Seat { get; set; }
+        public bool Extra { get; set; }
+        public int SkinReplacement { get; set; } = 300;
+    }
+}
